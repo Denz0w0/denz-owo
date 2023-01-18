d89fd253425b048de86830cbdb7762ed928a8044
Denz0w0 <123026844+Denz0w0@users.noreply.github.com>
2023-01-18 16:50:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
-pkg update && pkg upgrade
-pkg install git -y
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
-pkg install npm
-git clone github.com/Denz0w0/denz-owo
-cd denz-owo
 npm install
-node .
+npm start
+
 
